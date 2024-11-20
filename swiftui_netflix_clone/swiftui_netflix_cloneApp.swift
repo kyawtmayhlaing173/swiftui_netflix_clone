cf5ebb936c08a102649e3aab1f8e0dab6e89f27d
kyawtmayhlaing173 <pinky.hlaing173@gmail.com>
2024-11-20 21:32:31 +0700

    Initial Commit

---
+++ swiftui_netflix_clone/swiftui_netflix_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_netflix_cloneApp.swift
+//  swiftui_netflix_clone
+//
+//  Created by Kyawt May Hlaing on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_netflix_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
